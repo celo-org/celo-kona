73a1bc012a5072a56593bfaa66025841b72b52f9
seolaoh <osa8361@gmail.com>
2025-05-16 21:31:51 +0900

    Add alloy-op-evm structs in Celo version

---
+++ crates/alloy-celo-evm/src/block/mod.rs
@@ -0,0 +1,48 @@
+//! Block executor for Celo.
+
+pub use receipt_builder::CeloAlloyReceiptBuilder;
+
+pub mod receipt_builder;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::CeloEvmFactory;
+    use alloy_consensus::{SignableTransaction, TxLegacy, transaction::Recovered};
+    use alloy_eips::{Encodable2718, eip2718::WithEncoded};
+    use alloy_evm::{EvmEnv, EvmFactory, block::BlockExecutor, block::BlockExecutorFactory};
+    use alloy_op_evm::{OpBlockExecutionCtx, OpBlockExecutorFactory};
+    use alloy_op_hardforks::OpChainHardforks;
+    use alloy_primitives::{Address, Signature};
+    use celo_revm::CeloTxEnvelope;
+    use revm::database::{CacheDB, EmptyDB, State};
+
+    #[test]
+    fn test_with_encoded() {
+        let executor_factory = OpBlockExecutorFactory::new(
+            CeloAlloyReceiptBuilder::default(),
+            OpChainHardforks::op_mainnet(),
+            CeloEvmFactory::default(),
+        );
+        let mut db = State::builder()
+            .with_database(CacheDB::<EmptyDB>::default())
+            .build();
+        let evm = executor_factory
+            .evm_factory()
+            .create_evm(&mut db, EvmEnv::default());
+        let mut executor = executor_factory.create_executor(evm, OpBlockExecutionCtx::default());
+        let tx = Recovered::new_unchecked(
+            CeloTxEnvelope::Legacy(TxLegacy::default().into_signed(Signature::new(
+                Default::default(),
+                Default::default(),
+                Default::default(),
+            ))),
+            Address::ZERO,
+        );
+        let tx_with_encoded = WithEncoded::new(tx.encoded_2718().into(), tx.clone());
+
+        // make sure we can use both `WithEncoded` and transaction itself as inputs.
+        let _ = executor.execute_transaction(&tx);
+        let _ = executor.execute_transaction(&tx_with_encoded);
+    }
+}
--- crates/alloy-celo-evm/src/lib.rs
+++ crates/alloy-celo-evm/src/lib.rs
@@ -1,1 +1,266 @@
 #![doc = include_str!("../README.md")]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+use alloy_evm::{Database, Evm, EvmEnv, EvmFactory};
+use alloy_primitives::{Address, Bytes, TxKind, U256};
+use celo_revm::{
+    CeloBuilder, CeloContext, CeloPrecompiles, CeloTransaction, CeloTxType, DefaultCelo,
+};
+use core::{
+    fmt::Debug,
+    ops::{Deref, DerefMut},
+};
+use op_alloy_consensus::OpTxType;
+use op_revm::{OpHaltReason, OpSpecId, OpTransaction, OpTransactionError};
+use revm::{
+    Context, ExecuteEvm, InspectEvm, Inspector,
+    context::{BlockEnv, TxEnv},
+    context_interface::result::{EVMError, ResultAndState},
+    handler::{PrecompileProvider, instructions::EthInstructions},
+    inspector::NoOpInspector,
+    interpreter::{InterpreterResult, interpreter::EthInterpreter},
+};
+
+pub mod block;
+
+/// Celo EVM implementation.
+///
+/// This is a wrapper type around the `revm` evm with optional [`Inspector`] (tracing)
+/// support. [`Inspector`] support is configurable at runtime because it's part of the underlying
+/// [`CeloEvm`](celo_revm::CeloEvm) type.
+#[allow(missing_debug_implementations)] // missing celo_revm::CeloContext Debug impl
+pub struct CeloEvm<DB: Database, I, P = CeloPrecompiles> {
+    inner:
+        celo_revm::CeloEvm<CeloContext<DB>, I, EthInstructions<EthInterpreter, CeloContext<DB>>, P>,
+    inspect: bool,
+}
+
+impl<DB: Database, I, P> CeloEvm<DB, I, P> {
+    /// Provides a reference to the EVM context.
+    pub const fn ctx(&self) -> &CeloContext<DB> {
+        &self.inner.0.0.data.ctx
+    }
+
+    /// Provides a mutable reference to the EVM context.
+    pub fn ctx_mut(&mut self) -> &mut CeloContext<DB> {
+        &mut self.inner.0.0.data.ctx
+    }
+
+    /// Provides a mutable reference to the EVM inspector.
+    pub fn inspector_mut(&mut self) -> &mut I {
+        &mut self.inner.0.0.data.inspector
+    }
+}
+
+impl<DB: Database, I, P> CeloEvm<DB, I, P> {
+    /// Creates a new Celo EVM instance.
+    ///
+    /// The `inspect` argument determines whether the configured [`Inspector`] of the given
+    /// [`CeloEvm`](celo_revm::CeloEvm) should be invoked on [`Evm::transact`].
+    pub const fn new(
+        evm: celo_revm::CeloEvm<
+            CeloContext<DB>,
+            I,
+            EthInstructions<EthInterpreter, CeloContext<DB>>,
+            P,
+        >,
+        inspect: bool,
+    ) -> Self {
+        Self {
+            inner: evm,
+            inspect,
+        }
+    }
+}
+
+impl<DB: Database, I, P> Deref for CeloEvm<DB, I, P> {
+    type Target = CeloContext<DB>;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.ctx()
+    }
+}
+
+impl<DB: Database, I, P> DerefMut for CeloEvm<DB, I, P> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.ctx_mut()
+    }
+}
+
+impl<DB, I, P> Evm for CeloEvm<DB, I, P>
+where
+    DB: Database,
+    I: Inspector<CeloContext<DB>>,
+    P: PrecompileProvider<CeloContext<DB>, Output = InterpreterResult>,
+{
+    type DB = DB;
+    type Tx = CeloTransaction<TxEnv>;
+    type Error = EVMError<DB::Error, OpTransactionError>;
+    type HaltReason = OpHaltReason;
+    type Spec = OpSpecId;
+
+    fn block(&self) -> &BlockEnv {
+        &self.block
+    }
+
+    fn chain_id(&self) -> u64 {
+        self.cfg.chain_id
+    }
+
+    fn transact_raw(
+        &mut self,
+        tx: Self::Tx,
+    ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
+        if self.inspect {
+            self.inner.set_tx(tx);
+            self.inner.inspect_replay()
+        } else {
+            self.inner.transact(tx)
+        }
+    }
+
+    fn transact_system_call(
+        &mut self,
+        caller: Address,
+        contract: Address,
+        data: Bytes,
+    ) -> Result<ResultAndState<Self::HaltReason>, Self::Error> {
+        let tx = CeloTransaction {
+            op_tx: OpTransaction {
+                base: TxEnv {
+                    caller,
+                    kind: TxKind::Call(contract),
+                    // Explicitly set nonce to 0 so revm does not do any nonce checks
+                    nonce: 0,
+                    gas_limit: 30_000_000,
+                    value: U256::ZERO,
+                    data,
+                    // Setting the gas price to zero enforces that no value is transferred as part of
+                    // the call, and that the call will not count against the block's
+                    // gas limit
+                    gas_price: 0,
+                    // The chain ID check is not relevant here and is disabled if set to None
+                    chain_id: None,
+                    // Setting the gas priority fee to None ensures the effective gas price is derived
+                    // from the `gas_price` field, which we need to be zero
+                    gas_priority_fee: None,
+                    access_list: Default::default(),
+                    // blob fields can be None for this tx
+                    blob_hashes: Vec::new(),
+                    max_fee_per_blob_gas: 0,
+                    tx_type: CeloTxType::NonCeloTx(OpTxType::Deposit).into(),
+                    authorization_list: Default::default(),
+                },
+                // The L1 fee is not charged for the EIP-4788 transaction, submit zero bytes for the
+                // enveloped tx size.
+                enveloped_tx: Some(Bytes::default()),
+                deposit: Default::default(),
+            },
+            fee_currency: None,
+        };
+
+        let mut gas_limit = tx.op_tx.base.gas_limit;
+        let mut basefee = 0;
+        let mut disable_nonce_check = true;
+
+        // ensure the block gas limit is >= the tx
+        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
+        // disable the base fee check for this call by setting the base fee to zero
+        core::mem::swap(&mut self.block.basefee, &mut basefee);
+        // disable the nonce check
+        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
+
+        let mut res = self.transact(tx);
+
+        // swap back to the previous gas limit
+        core::mem::swap(&mut self.block.gas_limit, &mut gas_limit);
+        // swap back to the previous base fee
+        core::mem::swap(&mut self.block.basefee, &mut basefee);
+        // swap back to the previous nonce check flag
+        core::mem::swap(&mut self.cfg.disable_nonce_check, &mut disable_nonce_check);
+
+        // NOTE: We assume that only the contract storage is modified. Revm currently marks the
+        // caller and block beneficiary accounts as "touched" when we do the above transact calls,
+        // and includes them in the result.
+        //
+        // We're doing this state cleanup to make sure that changeset only includes the changed
+        // contract storage.
+        if let Ok(res) = &mut res {
+            res.state.retain(|addr, _| *addr == contract);
+        }
+
+        res
+    }
+
+    fn db_mut(&mut self) -> &mut Self::DB {
+        &mut self.journaled_state.database
+    }
+
+    fn finish(self) -> (Self::DB, EvmEnv<Self::Spec>) {
+        let Context {
+            block: block_env,
+            cfg: cfg_env,
+            journaled_state,
+            ..
+        } = self.inner.0.0.data.ctx;
+
+        (journaled_state.database, EvmEnv { block_env, cfg_env })
+    }
+
+    fn set_inspector_enabled(&mut self, enabled: bool) {
+        self.inspect = enabled;
+    }
+}
+
+/// Factory producing [`CeloEvm`]s.
+#[derive(Debug, Default, Clone, Copy)]
+#[non_exhaustive]
+pub struct CeloEvmFactory;
+
+impl EvmFactory for CeloEvmFactory {
+    type Evm<DB: Database, I: Inspector<CeloContext<DB>>> = CeloEvm<DB, I>;
+    type Context<DB: Database> = CeloContext<DB>;
+    type Tx = CeloTransaction<TxEnv>;
+    type Error<DBError: core::error::Error + Send + Sync + 'static> =
+        EVMError<DBError, OpTransactionError>;
+    type HaltReason = OpHaltReason;
+    type Spec = OpSpecId;
+
+    fn create_evm<DB: Database>(
+        &self,
+        db: DB,
+        input: EvmEnv<OpSpecId>,
+    ) -> Self::Evm<DB, NoOpInspector> {
+        CeloEvm {
+            inner: Context::celo()
+                .with_db(db)
+                .with_block(input.block_env)
+                .with_cfg(input.cfg_env)
+                .build_celo_with_inspector(NoOpInspector {}),
+            inspect: false,
+        }
+    }
+
+    fn create_evm_with_inspector<DB: Database, I: Inspector<Self::Context<DB>>>(
+        &self,
+        db: DB,
+        input: EvmEnv<OpSpecId>,
+        inspector: I,
+    ) -> Self::Evm<DB, I> {
+        CeloEvm {
+            inner: Context::celo()
+                .with_db(db)
+                .with_block(input.block_env)
+                .with_cfg(input.cfg_env)
+                .build_celo_with_inspector(inspector),
+            inspect: true,
+        }
+    }
+}
