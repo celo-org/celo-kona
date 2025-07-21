aebcfa64b47d5b4e2ca9d1aeb79f8a03817be228
seolaoh <osa8361@gmail.com>
2025-07-22 00:13:05 +0900

    Adjust BootInfo to CeloBootInfo

--- crates/kona/proof/src/boot.rs
+++ crates/kona/proof/src/boot.rs
@@ -1,66 +1,36 @@
 //! This module contains the prologue phase of the client program, pulling in the boot information
 //! through the `PreimageOracle` ABI as local keys.
+//! TODO: When Celo network which we want to use for CI is included in [superchain-registry], we can
+//! remove `CeloBootInfo`.
 
-use crate::errors::OracleProviderError;
-use alloy_primitives::{B256, U256};
-use kona_genesis::RollupConfig;
+use alloy_primitives::B256;
+use celo_registry::ROLLUP_CONFIGS;
 use kona_preimage::{PreimageKey, PreimageOracleClient};
-use kona_registry::ROLLUP_CONFIGS;
+use kona_proof::{
+    BootInfo,
+    boot::{
+        L1_HEAD_KEY, L2_CHAIN_ID_KEY, L2_CLAIM_BLOCK_NUMBER_KEY, L2_CLAIM_KEY, L2_OUTPUT_ROOT_KEY,
+        L2_ROLLUP_CONFIG_KEY,
+    },
+    errors::OracleProviderError,
+};
 use serde::{Deserialize, Serialize};
 
-/// The local key ident for the L1 head hash.
-pub const L1_HEAD_KEY: U256 = U256::from_be_slice(&[1]);
-
-/// The local key ident for the L2 output root.
-pub const L2_OUTPUT_ROOT_KEY: U256 = U256::from_be_slice(&[2]);
-
-/// The local key ident for the L2 output root claim.
-pub const L2_CLAIM_KEY: U256 = U256::from_be_slice(&[3]);
-
-/// The local key ident for the L2 claim block number.
-pub const L2_CLAIM_BLOCK_NUMBER_KEY: U256 = U256::from_be_slice(&[4]);
-
-/// The local key ident for the L2 chain ID.
-pub const L2_CHAIN_ID_KEY: U256 = U256::from_be_slice(&[5]);
-
-/// The local key ident for the L2 rollup config.
-pub const L2_ROLLUP_CONFIG_KEY: U256 = U256::from_be_slice(&[6]);
-
 /// The boot information for the client program.
-///
-/// **Verified inputs:**
-/// - `l1_head`: The L1 head hash containing the safe L2 chain data that may reproduce the L2 head
-///   hash.
-/// - `agreed_l2_output_root`:The agreed upon safe L2 output root.
-/// - `chain_id`: The L2 chain ID.
-///
-/// **User submitted inputs:**
-/// - `claimed_l2_output_root`: The L2 output root claim.
-/// - `claimed_l2_block_number`: The L2 claim block number.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct BootInfo {
-    /// The L1 head hash containing the safe L2 chain data that may reproduce the L2 head hash.
-    pub l1_head: B256,
-    /// The agreed upon safe L2 output root.
-    pub agreed_l2_output_root: B256,
-    /// The L2 output root claim.
-    pub claimed_l2_output_root: B256,
-    /// The L2 claim block number.
-    pub claimed_l2_block_number: u64,
-    /// The L2 chain ID.
-    pub chain_id: u64,
-    /// The rollup config for the L2 chain.
-    pub rollup_config: RollupConfig,
+pub struct CeloBootInfo {
+    /// The boot information for OP.
+    pub op_boot_info: BootInfo,
 }
 
-impl BootInfo {
+impl CeloBootInfo {
     /// Load the boot information from the preimage oracle.
     ///
     /// ## Takes
     /// - `oracle`: The preimage oracle reader.
     ///
     /// ## Returns
-    /// - `Ok(BootInfo)`: The boot information.
+    /// - `Ok(CeloBootInfo)`: The boot information.
     /// - `Err(_)`: Failed to load the boot information.
     pub async fn load<O>(oracle: &O) -> Result<Self, OracleProviderError>
     where
@@ -74,7 +44,10 @@ impl BootInfo {
 
         let mut l2_output_root: B256 = B256::ZERO;
         oracle
-            .get_exact(PreimageKey::new_local(L2_OUTPUT_ROOT_KEY.to()), l2_output_root.as_mut())
+            .get_exact(
+                PreimageKey::new_local(L2_OUTPUT_ROOT_KEY.to()),
+                l2_output_root.as_mut(),
+            )
             .await
             .map_err(OracleProviderError::Preimage)?;
 
@@ -106,7 +79,7 @@ impl BootInfo {
         // Attempt to load the rollup config from the chain ID. If there is no config for the chain,
         // fall back to loading the config from the preimage oracle.
         let rollup_config = if let Some(config) = ROLLUP_CONFIGS.get(&chain_id) {
-            config.clone()
+            config.op_rollup_config.clone()
         } else {
             warn!(
                 target: "boot_loader",
@@ -121,12 +94,14 @@ impl BootInfo {
         };
 
         Ok(Self {
-            l1_head,
-            agreed_l2_output_root: l2_output_root,
-            claimed_l2_output_root: l2_claim,
-            claimed_l2_block_number: l2_claim_block,
-            chain_id,
-            rollup_config,
+            op_boot_info: BootInfo {
+                l1_head,
+                agreed_l2_output_root: l2_output_root,
+                claimed_l2_output_root: l2_claim,
+                claimed_l2_block_number: l2_claim_block,
+                chain_id,
+                rollup_config,
+            },
         })
     }
 }
