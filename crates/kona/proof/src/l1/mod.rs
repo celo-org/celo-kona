fd0eee5e117a3df8a28e8d598c59ca963457afbc
kourin <kourin.code@gmail.com>
2025-08-26 22:05:49 +0900

    Add CeloOracleL1ChainProvider

--- crates/kona/proof/src/l1/mod.rs
+++ crates/kona/proof/src/l1/mod.rs
@@ -1,4 +1,7 @@
 //! Contains the L2-specific constructs of the client program.
 
+mod chain_provider;
+pub use chain_provider::CeloOracleL1ChainProvider;
+
 mod pipeline;
 pub use pipeline::CeloOraclePipeline;
