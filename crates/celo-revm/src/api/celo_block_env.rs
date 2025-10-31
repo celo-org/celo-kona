86e9cda13856b04581643d3c3be96c5fe38400ef
Karl Bartel <karl.bartel@clabs.co>
2025-10-31 11:01:48 +0100

    Remove celo_block_env.rs

--- crates/celo-revm/src/api/celo_block_env.rs
+++
@@ -1,8 +0,0 @@
-use crate::fee_currency_context::FeeCurrencyContext;
-use op_revm::L1BlockInfo;
-
-#[derive(Debug, Clone, Default)]
-pub struct CeloBlockEnv {
-    pub l1_block_info: L1BlockInfo,
-    pub fee_currency_context: FeeCurrencyContext,
-}
